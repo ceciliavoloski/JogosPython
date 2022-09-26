3e13b8451987511f1ad51f4cc8db8bc1cd23b648
Cecilia de Oliveira Voloski <ceciliavoloski@gmail.com>
2022-09-25 23:32:37 -0300

    Adc import das funções dos jogos

--- menu-jogos.py
+++ menu-jogos.py
@@ -1,11 +1,20 @@
-print("**********************************")
-print("*********Escolha seu jogo*********")
-print("**********************************")
+import adivinhacao
+import forca
 
-print("(1) Adivinhação (2) Forca")
-jogo = int(input("Escolha seu jogo: "))
+def escolhe_jogo():
+	print("**********************************")
+	print("*********Escolha seu jogo*********")
+	print("**********************************")
 
-if(jogo == 1 ):
-	print("Você escolheu adivinhação")
-elif(jogo == 2):
-	print("Você escolheu o jogo da forca")
+	print("(1) Adivinhação (2) Forca")
+	jogo = int(input("Escolha seu jogo: "))
+
+	if(jogo == 1 ):
+		print("Você escolheu o jogo de adivinhação")
+		adivinhacao.jogar_adivinhacao()
+	elif(jogo == 2):
+		print("Você escolheu o jogo da forca")
+		forca.jogar_forca()
+
+if(__name__ == "__main__"):
+	escolhe_jogo()
